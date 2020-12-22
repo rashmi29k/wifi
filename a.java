5161dc351237c2fd23ab0ef3da24e2bc27cda776
gitserver <rashmi29491@gmail.com>
2020-12-22 14:18:07 +0530

    adding new branch

--- a.java
+++ a.java
@@ -1,7 +1,5 @@
-adding new branch
-B
-class a{
-public static void main(String args[]){
+//adding new branch
+main(String args[]){
 System.out.println("Hello, World!");
   //adding the line for cherry-pick
 }
